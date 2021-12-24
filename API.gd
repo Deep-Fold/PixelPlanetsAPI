fc3867c2a73ee48401af4ea02b8f4cbc6cfdcded
Deep-Fold <marijn.martin@gmail.com>
2021-12-24 12:16:47 +0100

    Initial commit

---
+++ API.gd
@@ -0,0 +1,72 @@
+extends Node
+
+var gui
+
+enum export_mode {NONE, IMG, GIF, SPRITESHEET}
+var planet_types = {
+	TerranWet = "Terran Wet",
+	TerranDry = "Terrand Dry",
+	Islands = "Island",
+	NoAtmosphere = "No atmosphere",
+	GasGiant1 = "Gas giant 1",
+	GasGiant2 = "Gas giant 2",
+	IceWorld = "Ice World",
+	LavaWorld = "Lava World",
+	Asteroid = "Asteroid",
+	BlackHole = "Black Hole",
+	Galaxy = "Galaxy",
+	Star = "Star",
+}
+
+func _ready():
+	gui = get_tree().root.get_node("GUI")
+	yield(get_tree(), "idle_frame")
+	
+	# some examples of how you could use it.
+#	_create_planet(planet_types.TerranWet, export_mode.IMG, 34207432, 200, PI * 0.5, Vector2(0.75,0.75), true, {})
+#	_create_planet(planet_types.TerranWet, export_mode.SPRITESHEET, 34207432, 200, PI * 0.5, Vector2(0.75,0.75), true, {"frames_w": 20, "frames_h": 5})
+#	_create_planet(planet_types.TerranWet, export_mode.GIF, 34207432, 100, PI * 0.5, Vector2(0.75,0.75), true, {"frames": 300, "length": 5})
+
+
+
+
+# create a planet with the following params
+# type (String) - choose from planet_types, for example: planet_types.TerranWet .
+# export mode (enum) - image will be saved according to export type. To not export, use export_mode.NONE.
+# seed (int) - unique seed of the planet. leave 0 for random
+# pixels (int) - amount of pixels planet will be rendered in (Black Hole and Gas Planet 2 will use more pixels than given because of their rings).
+# rotation (float) - radial rotation of planet.
+# light_origin (Vector2) - where planet will be lit from, from Vector2(0,0) (top left), to Vector2(1,1) (bottom right).
+# dither (bool) - whether or not to dither in image.
+# colors (PoolColorArray) - colorscheme that will be used for planet. Leave empty for random
+# export_options (Dictionary) - options for either spritesheet or gif, containing follow info:
+# { "frames_w": int, "frames_h": int} for spritesheets
+# { "frames": int, "length": float} for gifs (length being length of gif in seconds).
+func _create_planet(type, mode = export_mode.NONE, sd = 0, pixels = 100, rotation = 0, light_origin = Vector2(0.4,0.4),
+ dither = true, export_options = {}, colors = PoolColorArray()):
+	if sd == 0:
+		gui._seed_random()
+	else:
+		gui.sd = sd
+		
+	var t = gui._lookup_type(type)
+	gui._create_new_planet(t)
+	gui._set_pixels(pixels)
+	gui.rotate_planet(rotation)
+	gui.set_light_origin(light_origin)
+	gui.set_dither(dither)
+	
+	yield(get_tree(), "idle_frame")
+	if colors.size() < 1:
+		gui._randomize_colors()
+	else:
+		gui.set_colors(colors)
+	
+	yield(get_tree(), "idle_frame")
+	match mode:
+		export_mode.IMG:
+			gui._export_img()
+		export_mode.SPRITESHEET:
+			gui.export_spritesheet_no_bar(Vector2(export_options.frames_w, export_options.frames_h))
+		export_mode.GIF:
+			gui.export_gif_no_bar(export_options.frames, float(export_options.length) / export_options.frames)
---
+++ GUI/GUI.gd
@@ -0,0 +1,388 @@
+extends Control
+
+onready var viewport = $PlanetViewport
+onready var viewport_planet = $PlanetViewport/PlanetHolder
+onready var viewport_holder = $PlanetHolder
+onready var viewport_tex = $PlanetHolder/ViewportTexture
+onready var seedtext = $Settings/VBoxContainer/Seed/SeedText
+onready var optionbutton = $Settings/VBoxContainer/OptionButton
+onready var colorholder = $Settings/VBoxContainer/ColorButtonHolder
+onready var picker = $Panel/ColorPicker
+onready var random_colors = $Settings/VBoxContainer/HBoxContainer/RandomizeColors
+onready var dither_button = $Settings/VBoxContainer/HBoxContainer2/ShouldDither
+
+onready var colorbutton_scene = preload("res://GUI/ColorPickerButton.tscn")
+
+
+const GIFExporter = preload("res://addons/gdgifexporter/exporter.gd")
+const MedianCutQuantization = preload("res://addons/gdgifexporter/quantization/median_cut.gd")
+
+onready var planets = {
+	"Terran Wet": preload("res://Planets/Rivers/Rivers.tscn"),
+	"Terran Dry": preload("res://Planets/DryTerran/DryTerran.tscn"),	
+	"Islands": preload("res://Planets/LandMasses/LandMasses.tscn"),
+	"No atmosphere": preload("res://Planets/NoAtmosphere/NoAtmosphere.tscn"),
+	"Gas giant 1": preload("res://Planets/GasPlanet/GasPlanet.tscn"),
+	"Gas giant 2": preload("res://Planets/GasPlanetLayers/GasPlanetLayers.tscn"),
+	"Ice World": preload("res://Planets/IceWorld/IceWorld.tscn"),
+	"Lava World": preload("res://Planets/LavaWorld/LavaWorld.tscn"),
+	"Asteroid": preload("res://Planets/Asteroids/Asteroid.tscn"),
+	"Black Hole": preload("res://Planets/BlackHole/BlackHole.tscn"),
+	"Galaxy": preload("res://Planets/Galaxy/Galaxy.tscn"),
+	"Star": preload("res://Planets/Star/Star.tscn"),
+}
+var pixels = 100.0
+var scale = 1.0
+var sd = 0
+var colors = []
+var should_dither = true
+
+func _ready():
+	for k in planets.keys():
+		optionbutton.add_item(k)
+
+	_seed_random()
+	_create_new_planet(planets["Terran Wet"])
+#	yield(get_tree(), "idle_frame")
+#	viewport.size = Vector2(pixels, pixels)
+
+func _lookup_type(t):
+	return planets[t]
+
+func _on_OptionButton_item_selected(index):
+	var chosen = planets[planets.keys()[index]]
+	_create_new_planet(chosen)
+	_close_picker()
+
+func _on_SliderRotation_value_changed(value):
+	rotate_planet(value)
+
+func rotate_planet(r):
+	viewport_planet.get_child(0).set_rotate(r)
+
+func _on_Control_gui_input(event):
+	if (event is InputEventMouseMotion || event is InputEventScreenTouch) && Input.is_action_pressed("mouse"):
+		var normal = event.position / Vector2(300, 300)
+		set_light_origin(normal)
+		
+		if $Panel.visible:
+			_close_picker()
+
+func set_light_origin(l):
+	viewport_planet.get_child(0).set_light(l)
+
+func _on_LineEdit_text_changed(new_text):
+	call_deferred("_make_from_seed", int(new_text))
+
+func _make_from_seed(new_seed):
+	sd = new_seed
+	seed(sd)
+	viewport_planet.get_child(0).set_seed(sd)
+
+func _create_new_planet(type):
+	for c in viewport_planet.get_children():
+		c.queue_free()
+	
+	var new_p = type.instance()
+	viewport_planet.add_child(new_p)
+	
+	seed(sd)
+	new_p.set_seed(sd)
+	new_p.set_pixels(pixels)
+	new_p.rect_position = pixels * 0.5 * (new_p.relative_scale -1) * Vector2(1,1)
+	new_p.set_dither(should_dither)
+	
+	colors = new_p.get_colors()
+	_make_color_buttons()
+
+	yield(get_tree(), "idle_frame")
+	viewport.size = Vector2(pixels, pixels) * new_p.relative_scale
+	
+	# some hardcoded values that look good in the GUI
+	match new_p.relative_scale:
+		1.0:
+			viewport_tex.rect_position = Vector2(50,50)
+			viewport_tex.rect_size = Vector2(200,200)
+##			viewport_tex.rect_scale = Vector2(2.0,2.0)
+		2.0:
+			viewport_tex.rect_position = Vector2(25,25)
+			viewport_tex.rect_size = Vector2(250,250)
+		2.5:
+			viewport_tex.rect_position = Vector2(0,0)
+			viewport_tex.rect_size = Vector2(300,300)
+
+func set_dither(d):
+	viewport_planet.get_child(0).set_dither(d)
+
+func _make_color_buttons():
+	for b in colorholder.get_children():
+		b.queue_free()
+	
+	for i in colors.size():
+		var b = colorbutton_scene.instance()
+		b.set_color(colors[i])
+		b.set_index(i)
+		b.connect("color_picked", self, "_on_colorbutton_color_picked")
+		b.connect("button_pressed", self, "_on_colorbutton_pressed")
+		picker.connect("color_changed", b, "_on_picker_color_changed")
+		
+		colorholder.add_child(b)
+
+func _on_colorbutton_pressed(button):
+	for b in colorholder.get_children():
+		b.is_active = false
+	button.is_active = true
+	$Panel.visible = true
+	picker.color = button.own_color
+
+func _on_colorbutton_color_picked(color, index):
+	colors[index] = color
+	viewport_planet.get_child(0).set_colors(colors)
+
+func set_colors(colors):
+	viewport_planet.get_child(0).set_colors(colors)
+
+func _seed_random():
+	randomize()
+	sd = randi()
+	seed(sd)
+	seedtext.text = String(sd)
+	viewport_planet.get_child(0).set_seed(sd)
+
+func _on_Button_pressed():
+	_seed_random()
+
+func _on_ExportPNG_pressed():
+	_export_img()
+
+func _export_img():
+	var planet = viewport_planet.get_child(0)
+	var tex = viewport.get_texture().get_data()
+	var image = Image.new()
+	image.create(pixels * planet.relative_scale, pixels * planet.relative_scale, false, Image.FORMAT_RGBA8)
+	var source_xy = 0
+	var source_size = pixels*planet.relative_scale
+	var source_rect = Rect2(source_xy, source_xy,source_size,source_size)
+	image.blit_rect(tex, source_rect, Vector2(0,0))
+	
+	save_image(image)
+
+func export_spritesheet(sheet_size, progressbar):
+	var planet = viewport_planet.get_child(0)
+	var sheet = Image.new()
+	progressbar.max_value = sheet_size.x * sheet_size.y
+	
+	sheet.create(pixels * sheet_size.x * planet.relative_scale, pixels * sheet_size.y * planet.relative_scale, false, Image.FORMAT_RGBA8)
+	planet.override_time = true
+	
+	var index = 0
+	for y in range(sheet_size.y):
+		for x in range(sheet_size.x + 1):
+			planet.set_custom_time(lerp(0.0, 1.0, (index)/float((sheet_size.x+1) * sheet_size.y)))
+			yield(get_tree(), "idle_frame")
+			
+			if index != 0:
+				var image = viewport.get_texture().get_data()
+				var source_xy = 0
+				var source_size = pixels*planet.relative_scale
+				var source_rect = Rect2(source_xy, source_xy,source_size,source_size)
+				var destination = Vector2(x - 1,y) * pixels * planet.relative_scale
+				sheet.blit_rect(image, source_rect, destination)
+
+			index +=1
+			progressbar.value = index
+	
+	
+	planet.override_time = false
+	save_image(sheet)
+	$Popup.visible = false
+
+func export_spritesheet_no_bar(sheet_size):
+	var planet = viewport_planet.get_child(0)
+	var sheet = Image.new()
+	
+	sheet.create(pixels * sheet_size.x * planet.relative_scale, pixels * sheet_size.y * planet.relative_scale, false, Image.FORMAT_RGBA8)
+	planet.override_time = true
+	
+	var index = 0
+	for y in range(sheet_size.y):
+		for x in range(sheet_size.x + 1):
+			planet.set_custom_time(lerp(0.0, 1.0, (index)/float((sheet_size.x+1) * sheet_size.y)))
+			yield(get_tree(), "idle_frame")
+			
+			if index != 0:
+				var image = viewport.get_texture().get_data()
+				var source_xy = 0
+				var source_size = pixels*planet.relative_scale
+				var source_rect = Rect2(source_xy, source_xy,source_size,source_size)
+				var destination = Vector2(x - 1,y) * pixels * planet.relative_scale
+				sheet.blit_rect(image, source_rect, destination)
+
+			index +=1
+
+	planet.override_time = false
+	save_image(sheet)
+
+func save_image(img):
+	if OS.get_name() == "HTML5" and OS.has_feature('JavaScript'):
+		var filesaver = get_tree().root.get_node("/root/HTML5File")
+		filesaver.save_image(img, String(sd))
+	else:
+		if OS.get_name() == "OSX":
+			img.save_png("user://%s.png"%String(sd))
+		else:
+			img.save_png("res://%s.png"%String(sd))
+
+func _on_ExportSpriteSheet_pressed():
+	$Panel.visible = false
+	$Popup.visible = true
+	$Popup.set_pixels(pixels * viewport_planet.get_child(0).relative_scale)
+
+func _on_PickerExit_pressed():
+	_close_picker()
+
+func _close_picker():
+	$Panel.visible = false
+	for b in colorholder.get_children():
+		b.is_active = false
+
+
+func _on_RandomizeColors_pressed():
+	_randomize_colors()
+
+func _randomize_colors():
+	viewport_planet.get_child(0).randomize_colors()
+	colors = viewport_planet.get_child(0).get_colors()
+	for i in colorholder.get_child_count():
+		colorholder.get_child(i).set_color(colors[i])
+
+func _on_ResetColors_pressed():
+	viewport_planet.get_child(0).set_colors(viewport_planet.get_child(0).original_colors)
+	colors = viewport_planet.get_child(0).get_colors()
+	for i in colorholder.get_child_count():
+		colorholder.get_child(i).set_color(colors[i])
+
+func _on_ShouldDither_pressed():
+	should_dither = !should_dither
+	if should_dither:
+		dither_button.text = "On"
+	else:
+		dither_button.text = "Off"
+	viewport_planet.get_child(0).set_dither(should_dither)
+
+
+func _on_ExportGIF_pressed():
+	$GifPopup.visible = true
+	cancel_gif = false
+
+var cancel_gif = false
+func export_gif(frames, frame_delay, progressbar):
+	var planet = viewport_planet.get_child(0)
+	var exporter = GIFExporter.new(pixels*planet.relative_scale, pixels*planet.relative_scale)
+	progressbar.max_value = frames
+	
+	planet.override_time = true
+	planet.set_custom_time(0.0)
+	yield(get_tree(), "idle_frame")
+	
+	for i in range(frames):
+		if cancel_gif:
+			progressbar.value = 0
+			planet.override_time = false
+			break;
+			return;
+		
+		planet.set_custom_time(lerp(0.0, 1.0, float(i)/float(frames)))
+
+		yield(get_tree(), "idle_frame")
+		
+		var tex = viewport.get_texture().get_data()
+		var image = Image.new()
+		image.create(pixels * planet.relative_scale, pixels * planet.relative_scale, false, Image.FORMAT_RGBA8)
+		
+		var source_xy = 0
+		var source_size = pixels*planet.relative_scale
+		var source_rect = Rect2(source_xy, source_xy,source_size,source_size)
+		image.blit_rect(tex, source_rect, Vector2(0,0))
+		exporter.add_frame(image, frame_delay, MedianCutQuantization)
+		
+		progressbar.value = i
+	
+	if cancel_gif:
+		return
+	if OS.get_name() != "HTML5" or !OS.has_feature('JavaScript'):
+		var file: File = File.new()
+		if OS.get_name() == "OSX":
+			file.open("user://%s.gif"%String(sd), File.WRITE)
+		else:
+			file.open("res://%s.gif"%String(sd), File.WRITE)
+		file.store_buffer(exporter.export_file_data())
+		file.close()
+	else:
+		var fileName = String(sd)
+		var data = Array(exporter.export_file_data())
+		JavaScript.eval("downloadGif('%s', %s);" % [fileName, str(data)], true)
+
+	planet.override_time = false
+	$GifPopup.visible = false
+	progressbar.visible = false
+
+func export_gif_no_bar(frames, frame_delay):
+	var planet = viewport_planet.get_child(0)
+	var exporter = GIFExporter.new(pixels*planet.relative_scale, pixels*planet.relative_scale)
+
+	planet.override_time = true
+	planet.set_custom_time(0.0)
+	yield(get_tree(), "idle_frame")
+	
+	for i in range(frames):		
+		planet.set_custom_time(lerp(0.0, 1.0, float(i)/float(frames)))
+		yield(get_tree(), "idle_frame")
+		
+		var tex = viewport.get_texture().get_data()
+		var image = Image.new()
+		image.create(pixels * planet.relative_scale, pixels * planet.relative_scale, false, Image.FORMAT_RGBA8)
+		
+		var source_xy = 0
+		var source_size = pixels*planet.relative_scale
+		var source_rect = Rect2(source_xy, source_xy,source_size,source_size)
+		image.blit_rect(tex, source_rect, Vector2(0,0))
+		exporter.add_frame(image, frame_delay, MedianCutQuantization)
+
+	if cancel_gif:
+		return
+	if OS.get_name() != "HTML5" or !OS.has_feature('JavaScript'):
+		var file: File = File.new()
+		if OS.get_name() == "OSX":
+			file.open("user://%s.gif"%String(sd), File.WRITE)
+		else:
+			file.open("res://%s.gif"%String(sd), File.WRITE)
+		file.store_buffer(exporter.export_file_data())
+		file.close()
+	else:
+		var fileName = String(sd)
+		var data = Array(exporter.export_file_data())
+		JavaScript.eval("downloadGif('%s', %s);" % [fileName, str(data)], true)
+
+	planet.override_time = false
+
+func _on_GifPopup_cancel_gif():
+	cancel_gif = true
+
+func _on_InputPixels_text_changed(text):
+	pixels = int(text)
+	_set_pixels(pixels)
+
+func _set_pixels(pix):
+	pixels = clamp(pix, 12, 5000)
+	if (pix > 5000):
+		$Settings/VBoxContainer/InputPixels.text = String(pixels)
+	yield(get_tree(), "idle_frame")
+	var p = viewport_planet.get_child(0)
+	p.set_pixels(pixels)
+	
+	p.rect_position = pixels * 0.5 * (p.relative_scale -1) * Vector2(1,1)
+
+	var scl = p.relative_scale
+	yield(get_tree(), "idle_frame")
+	viewport.size = Vector2(pixels, pixels) * scl
